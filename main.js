c1f41b3d9536112de69f0f1804dcda057cae85b1
Casey Watts <casey.s.watts@gmail.com>
2017-07-28 05:02:02 -0400

    move notification to onClickTriggerNotification

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 const {app, Menu, Tray} = require('electron')
+const notify = require('electron-main-notification')
 
 let tray = null
 const onClickChangeIcon = function () {
   tray.setImage('arrows2.png');
+}
+const onClickTriggerNotification = function () {
+  notify('hi')
 }
 
 app.on('ready', () => {
   tray = new Tray('arrows.png')
   const contextMenu = Menu.buildFromTemplate([
     {label: 'change icon', click: onClickChangeIcon },
+    {label: 'trigger notification', click: onClickTriggerNotification },
     {role: 'quit'}
   ])
   tray.setToolTip('Electron Ping')
